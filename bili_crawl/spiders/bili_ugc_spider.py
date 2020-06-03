0e2a078733dcf1ef34d9c49750deb7371a254c76
z86588 <z86588@126.com>
2020-06-03 17:06:36 +0800

    init module

--- bili_crawl/spiders/bili_ugc_spider.py
+++ bili_crawl/spiders/bili_ugc_spider.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+from bili_crawl.items import UgcItem
+
 
 class BiliUgcSpiderSpider(scrapy.Spider):
     name = 'bili_ugc'
 
     allowed_domains = ['api.bilibili.com']
     start_urls = ['https://api.bilibili.com/x/web-interface/ranking?rid=0&day=3&type=1&arc_type=0&jsonp=jsonp']
+
     # start_urls = cl.ugc_uri
 
     def parse(self, response):
         body = response.body.decode('utf8')
         c_list = json.loads(body)['data']['list']
-        print(c_list[0])
+
+        for c in c_list:
+            item = UgcItem()
+            item['ugc_aid'] = c.get('aid')
+            print(item['ugc_aid'])
